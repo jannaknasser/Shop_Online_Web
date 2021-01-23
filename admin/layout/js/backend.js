af18972d285020077af198a76a2383fcacd3ba33
noornasser <jannaknasser@gmail.com>
2021-01-23 17:06:56 +0300

    admin files modifications

--- admin/layout/js/backend.js
+++ admin/layout/js/backend.js
@@ -2,6 +2,16 @@ $(function(){
 
 	'use strict';
 
+	$('.toggle-info').click(function (){
+
+		$(this).toggleClass('selected').parent().next('.panel-body').fadeToggle(100);
+		if ($(this).hasClass('selected')) {
+			$(this).html('<i class="fa fa-minus fa-lg"></i>');
+		}else{
+			$(this).html('<i class="fa fa-plus fa-lg"></i>');
+		}
+	});
+
 	// Trigger the SelectBox  
 	$("select").selectBoxIt({
 		autoWidth:false;
